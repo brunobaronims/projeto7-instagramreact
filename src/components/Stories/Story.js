1da81a2f7742994b6e71b21e8cda8d0b7772b8aa
Bruno Baroni <brunobaronims@gmail.com>
2022-09-16 18:45:19 -0300

    added navbar styling

--- src/components/Stories/Story.js
+++ src/components/Stories/Story.js
@@ -1,7 +1,10 @@
 export default function Story({ data }) {
   return (
     <li>
-      <img alt="Story" src={data.image} />
+      <div className='story-image'>
+        <img className='story-background' alt='Background' src='./img/story-circle.svg' />
+        <img alt="Story" src={data.image} />
+      </div>
       <p>{data.profile}</p>
     </li>
   );
